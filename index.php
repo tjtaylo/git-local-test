02bd8de0920aead17a0d3650b6be7b0b8bcd1c6b
Travis Taylor <travis@webdesignofyork.com>
2014-03-02 21:43:52 -0500

    add sidebar

--- index.php
+++ index.php
@@ -7,5 +7,7 @@
 <body>
 	<h2>This is a git test</h2>
 	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, obcaecati, modi, assumenda, ullam asperiores natus fugiat aperiam quod perspiciatis eos tenetur quia consequuntur. In provident corporis eveniet molestiae adipisci quam.</p>	
+
+	<div id="sidebar">Another sidebar from branch sidebar</div>
 </body>
 </html>
